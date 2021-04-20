d5619e400887dbe7bbb7469c5a6349e8e95edd83
Akighan <Akighan@mail.ru>
2021-04-20 23:23:50 +0300

    add Block class

---
+++ app/src/main/java/com/github/akighan/tetris/constants/FieldConstants.kt
@@ -0,0 +1,6 @@
+package com.github.akighan.tetris.constants
+
+enum class FieldConstants(val value: Int) {
+    COLUMN_COUNT(10), ROW_COUNT(20);
+
+}
--- app/src/main/java/com/github/akighan/tetris/models/Block.java
+++ app/src/main/java/com/github/akighan/tetris/models/Block.java
@@ -3,12 +3,73 @@ package com.github.akighan.tetris.models;
 import android.graphics.Color;
 import android.graphics.Point;
 
+import androidx.annotation.NonNull;
+
+import com.github.akighan.tetris.constants.FieldConstants;
+
+import java.util.Random;
+
 public class Block {
     private int shapeIndex;
     private int frameNumber;
     private BlockColor color;
     private Point position;
 
+    private Block (int shapeIndex, BlockColor blockColor) {
+        this.frameNumber = 0;
+        this.shapeIndex = shapeIndex;
+        this.color = blockColor;
+        this.position =  new Point(FieldConstants.COLUMN_COUNT.getValue(), 0);
+    }
+
+    public static Block createBlock() {
+        Random random = new Random();
+        int shapeIndex = random.nextInt(Shape.values().length);
+        BlockColor blockColor = BlockColor.values() [random.nextInt(BlockColor.values().length)];
+
+        Block block = new Block(shapeIndex, blockColor);
+        block.position.x = block.position.x - Shape.values() [shapeIndex].getStartPosition();
+        return block;
+    }
+
+    public static int getColor (byte value) {
+        for (BlockColor colour : BlockColor.values()) {
+            if (value == colour.byteValue) {
+                return colour.rgbValue;
+            }
+        }
+        return -1;
+    }
+
+    public final void setState (int frame, Point position) {
+        this.frameNumber = frame;
+        this.position = position;
+    }
+
+    @NonNull
+    public final byte [][] getShape (int frameNumber) {
+        return Shape.values() [shapeIndex].getFrame(frameNumber).as2dByteArray();
+    }
+
+    public Point getPosition() {
+        return this.position;
+    }
+
+    public final int getFrameCount () {
+        return Shape.values() [shapeIndex].getFrameCount();
+    }
+
+    public int getFrameNumber () {
+        return frameNumber;
+    }
+
+    public int getColor() {
+        return color.rgbValue;
+    }
+
+    public byte getStaticValue() {
+        return color.byteValue;
+    }
 
     public enum BlockColor {
         PINK(Color.rgb(255, 105, 180), (byte) 2),
@@ -24,6 +85,5 @@ public class Block {
 
         private final int rgbValue;
         private final byte byteValue;
-        //TODO p. 123
     }
 }
